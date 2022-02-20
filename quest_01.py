406b46e33d283146e881bc0a7845bda3c9324513
NST-solucao <74821264+NST-solucao@users.noreply.github.com>
2022-02-20 04:40:11 -0300

    Update quest_01.py
    Incluindo mais comentários explicando a situação da primeira linha variável e

--- quest_01.py
+++ quest_01.py
@@ -1,6 +1,6 @@
 
-e = '     ******' # O manipulação desse str está relacionado com a variavel n sendo assim 
+e = '     ******' # O manipulação desse str está relacionado com a variavel n sendo assim. sendo assim, para cada n+1 precisa incluir +1 espaço e +1 *
 n = 6
 
-for i in range(n):
+for i in range(n): 
     print(e[0+i:n+i])
