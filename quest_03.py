f4c5867a8ac1fad42561add9532a72fb6512b542
NST-solucao <74821264+NST-solucao@users.noreply.github.com>
2022-02-28 00:15:52 -0300

    Update quest_03.py

--- quest_03.py
+++ quest_03.py
@@ -1,16 +1,15 @@
-# import itertools as it
+palavra = input('Digite uma palavra : ')
 
-anagrama = input('Digite 1 palavra : ')
-a = sorted(anagrama)
-t = len(a) # Só para gerar de acordo com numero de caracteres digitado para contagem no FOR 
-# s = list(it.combinations(anagrama, 2))
-# s1 = ''.join(anagrama)
-# l = list(zip(it.count(), anagrama))
+anagramas = sorted(palavra)
+t = len(anagramas)
+count = 0
 
-for i in range(t):
-    # print(f'Valor do i = {a[i] + a[i-1] + a[i-2]}')
-    if a[i] == a[i-1]:
-        print(f'Encontrado anagramas dentro da palavra {anagrama}. A lista de todos os anagramas pares são:[{a[i]},{a[i-1]}] que estão nas posições: : [{anagrama.index(a[i])}, {anagrama.index(a[i-1])}]')
-    if  a[i] + a[i-1] + a[i-2] in anagrama:
-        print(f'A lista de todos os anagramas de 3 letras são: [{a[i]} , {a[i-1]} , {a[i-2]}] que estão nas posições : [{anagrama.index(a[i])}, {anagrama.index(a[i-1])}, {anagrama.index(a[i-2])}]')
-
+for i in palavra:
+    if anagramas[count-1] == anagramas[count]: 
+        print(f'A lista de todos os anagramas com 1 letra é:[{anagramas[count-1]} , {anagramas[count]}] que estão nas posições : ')
+    if sorted(palavra[count:count+2]) == sorted(palavra[count+1:count+3]):
+        print(f'A lista de todos os anagramas com 2 letras são: [{palavra[count:count+2]},{palavra[count+1:count+3]}] que estão nas posições : ')
+    if sorted(palavra[count:count+3]) == sorted(palavra[count+1:count+4]):
+        print(f'A lista de todos os anagramas com 3 letras são: [{palavra[count:count+3]},{palavra[count+1:count+4]}] que estão nas posições :')
+    
+    count += 1
