0f77e975de9fe8a3e40a446f06063d3bd08aaea2
NST-solucao <74821264+NST-solucao@users.noreply.github.com>
2022-02-28 19:40:19 -0300

    Update quest_03.py
    ultima atualização devido o retorno das quantidades de anagrama em pares.

--- quest_03.py
+++ quest_03.py
@@ -1,15 +1,20 @@
 palavra = input('Digite uma palavra : ')
 
 anagramas = sorted(palavra)
-#t = len(anagramas)
+t = len(anagramas)
 count = 0
+contnum = 0
 
 for i in palavra:
     if anagramas[count-1] == anagramas[count]: 
-        print(f'A lista de todos os anagramas com 1 letra é:[{anagramas[count-1]} , {anagramas[count]}] que estão nas posições : ')
+        # print(f'A lista de todos os anagramas com 1 letra é:[{anagramas[count-1]} , {anagramas[count]}] que estão nas posições : ')
+        contnum += 1
     if sorted(palavra[count:count+2]) == sorted(palavra[count+1:count+3]):
-        print(f'A lista de todos os anagramas com 2 letras são: [{palavra[count:count+2]},{palavra[count+1:count+3]}] que estão nas posições : [ , ] [ , ]')
+        # print(f'A lista de todos os anagramas com 2 letras são: [{palavra[count:count+2]},{palavra[count+1:count+3]}] que estão nas posições : ')
+        contnum += 1
     if sorted(palavra[count:count+3]) == sorted(palavra[count+1:count+4]):
-        print(f'A lista de todos os anagramas com 3 letras são: [{palavra[count:count+3]},{palavra[count+1:count+4]}] que estão nas posições : [ , , ] [ , , ] ')
-    
+        # print(f'A lista de todos os anagramas com 3 letras são: [{palavra[count:count+3]},{palavra[count+1:count+4]}] que estão nas posições :')
+        contnum += 1
     count += 1
+
+print(f'Foi encontrado {contnum} pares de anagramas na palavra {palavra} ')
